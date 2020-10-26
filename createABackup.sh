508bc976942bc496b77e57ffb59e59dbf505c300
Levijs <levijskal@icloud.com>
2020-10-25 20:55:11 -0400

    Create createABackup.sh

---
+++ createABackup.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+#T he path of the folder to back up
+backup_files="/home/"
+
+# Backup destination pathdest="/tmp"
+
+# Create arhive filename
+currentDate=`date +%Y-%m-%d`
+hostname=$(hostname -s)
+archive_file="$hostname-$currentDate.tgz"
+
+# Let the user know that the process is starting
+echo -e "backing up $backup_files to $dest/$archive_file \nprocess."
+
+# Print the start dateecho "Script start time: `date`"
+
+# Break line
+# Back up the files using tar command
+tar czfP $dest/$archive_file $backup_files
+
+# Print the end status message
+echo -e "Backup finished! \nScript end time: `date`"
